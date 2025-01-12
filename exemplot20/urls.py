db032a44de3eff107a52fc5ec3a1534f699dd7dc
Zacarias F Lima <zaca_lima@hotmail.com>
2025-01-11 23:21:32 -0300

    v.0.0.2 - Templates

--- exemplot20/urls.py
+++ exemplot20/urls.py
@@ -15,15 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
-
-#Temp Functions
-#-- Home
-def home (request):
-    return HttpResponse ('Home')
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home), #home
+    path('', include('characters.urls')),
 ]
