93a16859513fd247fbd1f81032ae89a97c30bf3c
Zacarias F Lima <zaca_lima@hotmail.com>
2025-01-14 21:00:03 -0300

    v.0.0.3 - Header

--- characters/views.py
+++ characters/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home (request):
-    return render (request, 'characters/home.html')
+    return render (request, 'characters/pages/home.html')
